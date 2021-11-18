74a24124806444a68467bc28a81c8b4535db1438
Ricardo Deodutt <RicardoDeodutt@gmail.com>
2021-11-18 17:42:58 -0500

    Redesigning the format for the AWS Questions API

---
+++ kuralabsbot/question_bank.py
@@ -0,0 +1,22 @@
+question = {
+    1: {
+        "prompt": "Why is AWS more economical than traditional data centers for applications with varying compute workloads?",
+        "choices": {
+            "a": "Amazon EC2 costs are billed on a monthly basis.",
+            "b": "Users retain full administrative access to their Amazon EC2 instances.",
+            "c": "Amazon EC2 instances can be launched on demand when needed.",
+            "d": "Users can permanently run enough instances to handle peak workloads.",
+        },
+        "answer": ["a"],
+    },
+    2: {
+        "prompt": "Why is AWS more economical than traditional data centers for applications with varying compute workloads?",
+        "choices": {
+            "a": "Amazon EC2 costs are billed on a monthly basis.",
+            "b": "Users retain full administrative access to their Amazon EC2 instances.",
+            "c": "Amazon EC2 instances can be launched on demand when needed.",
+            "d": "Users can permanently run enough instances to handle peak workloads.",
+        },
+        "answer": ["a"],
+    },
+}
